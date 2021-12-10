1cff304016a17c15ca2a100eb7be6e1801648f2d
Szeryng <1376216197@qq.com>
2021-12-10 20:11:51 +0800

    BST::put

--- Search/include/BST.h
+++ Search/include/BST.h
@@ -1,19 +1,63 @@
 #pragma once
-template<typename T>
+#include <assert.h>
+#include <stack>
+
+//see p250
+
+template<typename Key,typename Value>
 struct Node {
-    int key;
-    T value;
+    Node(Key a, Value b) :key(a), value(b) {};
+
+    Key key;
+    Value value;
     int count = 0;
     Node* left = nullptr;
     Node* right = nullptr;
 };
 
-template<typename T>
+template<typename Key,typename Value>
 class BST {
-    typedef Node<T> Node;
+    typedef Node<Key,Value> Node;
+    typedef std::stack<Node*> stack;
 public:
+    ~BST() {};
+    Node& put(Key key, Value value);
 
 private:
     Node* head = nullptr;
-    size = 0;
+    int size = 0;
 };
+
+template<typename Key, typename Value>
+Node<Key,Value>& BST<Key,Value>::put(Key key, Value value) {
+    Node* ptr = new Node(key, value);
+    assert(ptr);
+    int k = 0;
+    stack stk;
+    if (!size) {
+        head = ptr;
+    }
+    else {
+        Node* tmp = head;
+        stk.push(tmp);
+        while (tmp->count > 1) {
+            if (tmp->left)
+                if (ptr->key < tmp->key)
+                    tmp = tmp->left;
+            if (tmp->right)
+                if (ptr->key > tmp->key)
+                    tmp = tmp->right;
+            stk.push(tmp);
+        }
+        ptr->key > tmp->key ? tmp->right = ptr : tmp->left = ptr;
+        while (!stk.empty()) {
+            k++;
+            Node* a;
+            stk.top()->count+=k;
+            stk.pop();
+        }
+    }
+    size++;
+    ptr->count++;
+    return *ptr;
+}
--- Search/src/main.cpp
+++ Search/src/main.cpp
@@ -1,7 +1,9 @@
 #include "BST.h"
 
+typedef BST<int, int> BST_int;
 int main() {
-
-
+	BST_int s;
+	for (int k = 0; k < 10; ++k)
+		s.put(k, k);
 	return 0;
 }
