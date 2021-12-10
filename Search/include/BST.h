e82272600e7d22c730ea1b7bb1e8c3bc052bbfbc
Szeryng <1376216197@qq.com>
2021-12-10 19:30:45 +0800

    Update BST.h

--- Search/include/BST.h
+++ Search/include/BST.h
@@ -1,10 +1,19 @@
 #pragma once
 template<typename T>
 struct Node {
-
+    int key;
+    T value;
+    int count = 0;
+    Node* left = nullptr;
+    Node* right = nullptr;
 };
 
 template<typename T>
 class BST {
+    typedef Node<T> Node;
+public:
 
+private:
+    Node* head = nullptr;
+    size = 0;
 };
